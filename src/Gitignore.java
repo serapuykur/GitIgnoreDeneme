f73ef25996b7a20e601e0f86f66b9cda1511dfe5
serapuykur <kagan.ozan1235@gmail.com>
2022-08-20 00:29:25 +0200

    M2

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -4,6 +4,7 @@ public class Gitignore {
         System.out.println("git ignore deneme 2");
         System.out.println("git ignore deneme branch");
         System.out.println("git ignore deneme master");
+        System.out.println("git ignore deneme master 2");
 
     }
 }
