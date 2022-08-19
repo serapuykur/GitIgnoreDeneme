dc7f882d58712cde3d6a3feb6065cceeeb766ed4
serapuykur <kagan.ozan1235@gmail.com>
2022-08-19 16:01:39 +0200

    B.1

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -3,6 +3,7 @@ public class Gitignore {
         System.out.println("git ignore deneme 1");
         System.out.println("git ignore deneme 2");
         System.out.println("git ignore deneme branch");
+        System.out.println("git ignore deneme branch 2");
 
     }
 }
